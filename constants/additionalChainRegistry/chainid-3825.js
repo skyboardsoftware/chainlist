7080515a6dbb6b57bc1f36ab3f3d30002f3dc3ac
Lenn Dolling <lenn.dolling@aroundtowngaragedoors.ca>
2025-09-17 09:54:07 -0700

    feat: Add SecurePact Testnet (Chain ID: 3825)
    Add SecurePact Testnet (Chain ID: 3825)

---
+++ constants/additionalChainRegistry/chainid-3825.js
@@ -0,0 +1,27 @@
+{
+  "name": "SecurePact Testnet",
+  "chain": "SPCT",
+  "rpc": [
+    "https://seed.spct.cc:4000/rpc"
+  ],
+  "faucets": [
+    "https://spct.cc/#faucet"
+  ],
+  "nativeCurrency": {
+    "name": "SecurePact Testnet",
+    "symbol": "SPCT",
+    "decimals": 8
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://spct.cc",
+  "shortName": "spct-testnet",
+  "chainId": 3825,
+  "networkId": 3825,
+  "icon": "securepact",
+  "explorers": [{
+    "name": "SecurePact Explorer",
+    "url": "https://spct.cc/#explorer",
+    "icon": "securepact",
+    "standard": "EIP3091"
+  }]
+}
